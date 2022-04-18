7a38f87f38b6d9e21c03955cfb956f02b1e5514c
Timothy Bogatyryov <tbbt17971@gmail.com>
2022-04-18 23:59:43 +0300

    Create 'Commit' entity

---
+++ Entities/Commit.ts
@@ -0,0 +1,20 @@
+import { generateCommitId } from './Utils/Factories';
+import { removeElementFromArray } from './Utils/Arrays';
+
+class Commit {
+    private id: string;
+    private descendants: Commit[];
+
+    constructor() {
+      this.id = generateCommitId();
+      this.descendants = [];
+    }
+
+    addDescendant(commit: Commit): void {
+        this.descendants.push(commit);
+    }
+
+    removeDescendant(commit: Commit): void {
+        removeElementFromArray(this.descendants, commit);
+    }
+}
